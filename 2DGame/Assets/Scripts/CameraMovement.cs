6f596c734d2274e28002de0d78dd3b7152996abb
Long Thai <n10006257@qut.edu.au>
2019-06-23 21:50:01 +1000

    Made camera follow player

---
+++ 2DGame/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	// Use LateUpdate because want camera to move last
+	// Sometimes if in update and move, the camera will move first
+	// before the player does causing it to look bad
+	void LateUpdate () {
+		if (transform.position != target.position) {
+			Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+			transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+		}
+	}
+}
