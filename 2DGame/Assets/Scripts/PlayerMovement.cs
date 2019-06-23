3153a626ada218fe544b6f12b1db883b1e10b33a
Long Thai <n10006257@qut.edu.au>
2019-06-23 20:07:18 +1000

    Created player with rigid body, movement, and animations
    Made player rigid body, collision box, blend trees and added animations on the player for idle and walking.

---
+++ 2DGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D playerRigidBody;
+	private Vector3 change;
+	private Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+		playerRigidBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		change = Vector3.zero;
+		// GetAxisRaw removes smoothing from normal GetAxis
+		change.x = Input.GetAxisRaw("Horizontal");
+		change.y = Input.GetAxisRaw("Vertical");
+		UpdateAnimationAndMove();
+	}
+
+	void UpdateAnimationAndMove() {
+		// If moving
+		if (change != Vector3.zero) {
+			MoveCharacter();
+			// Change idle animation
+			animator.SetFloat("moveX", change.x);
+			animator.SetFloat("moveY", change.y);
+			// Change walking animation
+			animator.SetBool("moving", true);
+		} else {
+			animator.SetBool("moving", false);
+		}
+	}
+
+	void MoveCharacter() {
+		playerRigidBody.MovePosition(
+			transform.position + change * speed * Time.deltaTime
+		);
+	}
+}
